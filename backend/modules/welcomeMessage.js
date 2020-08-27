5d8d480d32ac5bfc49e7e9f766030826f1f5b1e3
Sebastian Porling <sebastian.porling@gmail.com>
2020-08-27 21:38:47 +0200

    feat: added port, ip, and clear

--- backend/modules/welcomeMessage.js
+++ backend/modules/welcomeMessage.js
@@ -1,12 +1,19 @@
 const figlet = require("figlet");
+const clear = require("clear");
+const chalk = require("chalk");
+const internalIp = require('internal-ip');
 
 /**
  * Displays a welcome message
  */
-module.exports.display = () => {
+module.exports.display = (port) => {
     try {
-        console.log(figlet.textSync("vuetastic "));
-        console.log(figlet.textSync("real estate"));
+        clear();
+        console.log(chalk.greenBright(figlet.textSync("vuetastic ")));
+        console.log(chalk.cyan(figlet.textSync("real estate")));
+        console.log(chalk.magenta("\n A simple REST API for fetching real estate data!"))
+        console.log(chalk.yellowBright(`\n     Server on: http://${internalIp.v4.sync()}:${port}`));
+        console.log(chalk.redBright(`\t or on: http://localhost:${port}\n`));
     } catch (error) {
         console.log("vuetastic real estate");
     }
