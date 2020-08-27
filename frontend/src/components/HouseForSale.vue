61a66ad2f102674da6b1a3dd5f8ba6430e0cda2e
David Stjernqvist <david.stjernqvist@se.experis.com>
2020-08-27 16:39:55 +0200

    Updated house page

--- frontend/src/components/HouseForSale.vue
+++ frontend/src/components/HouseForSale.vue
@@ -3,54 +3,56 @@
       <h1>HouseForSale</h1>
 
       <div>House</div>
+      <div v-if="house.id!==undefined">
+
       
-      <img v-bind:src="getHouseImages()" class="img-thumbnail">
-      <div>{{house.listings[0].address}}</div>
-      <div>{{house.listings[0].description}}</div>
-      <div>{{house.listings[0].latitude}}</div>
-      <div>{{house.listings[0].longitude}}</div>
+      <div v-for="image in house.images" v-bind:key="image.id">
+          <img v-bind:src="image" class="img-thumbnail">
+      </div>
+      
+      <div>{{house.address}}</div>
+      <div>{{house.description}}</div>
+      <div>{{house.latitude}}</div>
+      <div>{{house.longitude}}</div>
 
       <h2>Estate agent</h2>
-      <img v-bind:src="getEstateAgentImageSrc()" class="img-thumbnail">
-      <div>{{house.first_name}} {{house.last_name}}</div>
-      <div>{{house.email}}</div>
+      <img v-bind:src="house.agent.profile_image" class="img-thumbnail">
+      <div>{{house.agent.first_name}} {{house.agent.last_name}}</div>
+      <div>{{house.agent.email}}</div>
+      </div>
+  
   </div>
 </template>
-
 <script>
-import house_data from '../assets/house_data.json';
-
+import axios from 'axios';
+//gzH6AQwUg
+//AJMgtqi-hB - 2 images
 export default {
     name: "HouseForSale",
     data(){
         return{
-            house: house_data[0],
-            n: 0,
+            house: {},
             houseId: ""
-        }
+             }
     },
-    created(){
+    async created(){
         this.houseId = this.$route.params.id;
+        await this.getHouseObject();
     },
     mounted(){
-        console.log(this.$route.params.id);
-        console.log(this.house);
+        
     },
     methods: {
-        getHouseImages: function() {
-            return this.house.listings[this.n].images[0];
-        },
-        getEstateAgentImageSrc: function(){
-            return this.house.profile_image;
-        },
-        findObject: function(){
-
+        getHouseObject: async function(){
+            let response = await axios.get(`http://localhost:3000/api/house/${this.houseId}`);
+            this.house = response.data;
+            console.log(this.house);
         }
     }
 }
 </script>
 
-<style>
+<style scoped>
 img{
     max-width: 100%;
     height: auto;
