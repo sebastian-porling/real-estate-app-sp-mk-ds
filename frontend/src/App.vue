256880b0b6be29a2fd88f77031f7f709d3a8dcdb
David Stjernqvist <david.stjernqvist@se.experis.com>
2020-08-27 17:01:12 +0200

    Removed commented code

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,13 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
   
         <router-view></router-view>
-=======
-    <HousesForSaleList></HousesForSaleList>
->>>>>>> 47ff299dc0f08c91583bb7cebc7ac256252f0d4a
   </div>
 </template>
 
 <script>
-import HousesForSaleList from './components/HousesForSaleList.vue'
 export default {
-  name: 'App',
-  components: {
-     HousesForSaleList
-  }
+  name: 'App'
 }
 </script>
 
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,18 +4,20 @@ import App from './App.vue'
 import VueRouter from 'vue-router';
 
 import HouseForSale from "./components/HouseForSale.vue";
-
+import HousesForSaleList from './components/HousesForSaleList.vue';
 Vue.use(VueRouter);
 
 Vue.config.productionTip = false
 
 
 const routes = [
-  {path: '/house/:id', component: HouseForSale}
+  {path: '/house/:id', component: HouseForSale},
+  {path: '/', component: HousesForSaleList}
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode:"history"
 })
 //render: h => h(App),
 new Vue({
