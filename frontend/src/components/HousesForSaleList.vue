464dd91ba809e8671d5eaf7337711e42f5a07e63
Mehtab Kayani <mehtab.kayani@se.experis.com>
2020-08-27 16:21:58 +0200

    added functionallity to fetch house data and to display the data

--- frontend/src/components/HousesForSaleList.vue
+++ frontend/src/components/HousesForSaleList.vue
@@ -1,48 +1,47 @@
 <template>
   <div>
-      <h1>Houses for sale!</h1>
+    <h1>Houses for sale!</h1>
 
-    <div v-for="agent in houseData" :key="agent.id">
-       
-        <img v-bind:src="agent.profile_image" /> 
-        <a href="#"><h3>{{agent.first_name}} {{agent.last_name}}</h3></a>
-        <img v-bind:src="agent.listings[0].images[0]" /> 
-        
-        <p>{{agent.listings[0].address}}</p>
-        <a href="#"></a>
-        <hr>
+    <div v-for="house in houseData" :key="house.id">
+      <h3>Address</h3>
+      <a href="#"
+        ><p>{{ house.address }}</p></a
+      >
+      <img v-bind:src="house.images[0]" />
+      <h3>Real Estate Agent</h3>
+      <a href="#"
+        ><p>{{ house.agent.first_name }} {{ house.agent.last_name }}</p>
+
+        <img v-bind:src="house.agent.profile_image" />
+      </a>
+
+      <hr />
     </div>
-
-    <img src="https://thispersondoesnotexist.com/" alt="">Image
-   
-
-
-      
-      
-  
   </div>
-  
 </template>
 
 <script>
-// eslint-disable-next-line no-unused-vars
-import HouseData from "../../public/house_data.json";
+
+import axios from "axios";
+
 export default {
-    data() {
-        return {
-            
-            houseData: []
+  data() {
+    return {
+      houseData: [],
     };
   },
-  mounted(){
-      this.houseData=HouseData
-           
-    }
+  async created() {
+    let response = await axios.get("http://localhost:3000/api/houses");
+    this.houseData = response.data;
+    console.log(response.data);
+  },
+  mounted() {
+  },
 };
 </script>
 
 <style scoped>
-hr{
-    max-width: 250px;
+hr {
+  max-width: 250px;
 }
 </style>
