db1b1db70c07f2cd1202a58c941adedfa874bf58
Sebastian Porling <sebastian.porling@gmail.com>
2020-08-27 15:13:15 +0200

    feat: express, houses, house/id and agent/id

---
+++ backend/modules/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const { logger } = require("./logger.js");
+const House = require('./house.js');
+const app = express();
+
+/* register logger */
+app.use(logger);
+
+/**
+ * Gets all the houses
+ */
+app.get("/api/houses", (req, res) => {
+    res.status(200).json(House.getAll());
+});
+
+/**
+ * Gets the house on the given id
+ */
+app.get("/api/house/:id", (req, res) => {
+    res.status(200).json(House.get(req.params.id));
+})
+
+/**
+ * Gets the agent on the given id
+ */
+app.get("/api/agent/:id", (req, res) => {
+    res.status(200).json(House.getAgent(req.params.id));
+});
+
+/**
+ * Handles request for all other URLS
+ */
+app.get("*", (req, res) => {
+    res.status(404).json({error: 404, message: "This api doesn't support this url."});
+});
+
+/**
+ * Starts the server at the given port
+ * @param port 
+ */
+module.exports.start = (port) => {
+    app.listen(port, () => console.log(`Listening on port ${port}`));
+};
