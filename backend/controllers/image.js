802105a49cccabadaae1cc7a4c29d71e94143297
Sebastian Porling <sebastian.porling@gmail.com>
2020-08-28 09:02:11 +0200

    doc: added documentation and comments

--- backend/controllers/image.js
+++ backend/controllers/image.js
@@ -1,8 +1,12 @@
 const express = require("express")
     , router = express();
 
+/* register static images */
 router.use("/", express.static("../images"));
 
+/**
+ * 404 for images that doesn't exist
+ */
 router.get("*", (req, res) => {
     res.status(404).json({ msg: "That image doesn't exist" });
 })
--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -2,14 +2,16 @@ const express = require("express")
     , router = express.Router()
     , path = require('path');
 
+/* register api, image and static pages */
 router.use("/api", require("./api"));
 router.use("/image", require("./image"));
 router.use("/", express.static(path.join(__dirname, "../dist")));
 
 /**
- * Handles request for all other URLS
+ * 404 for the pages that doesn't exist
  */
 router.get("*", (req, res) => {
+    /* TODO: change to static error html page */
     res.status(404).json({ msg: "The page doesn't exist" });
 });
 
