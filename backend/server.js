f441575096a3eca4e48d73200db300f8b06e56c7
Sebastian Porling <sebastian.porling@gmail.com>
2020-08-28 08:56:14 +0200

    feat: new simplified server

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+    , app = express();
+
+/* register logger and body parser */
+app.use(require("./util/logger"));
+app.use(express.json());
+
+/* register routes and middleware */
+app.use(require("./middlewares"));
+app.use(require("./controllers"));
+
+/**
+ * Starts the server at the given port
+ * @param port
+ */
+module.exports.start = (port) => {
+    app.listen(port);
+};
