f3ca0f95af14fed458cab07b6dcb807da3203aff
Sebastian Porling <sebastian.porling@gmail.com>
2020-08-30 18:36:12 +0200

    using Jimp for fetch img, add style for cmd Using figlet, clear, chalk and ora for styling the cmd

--- backend/images/houses/houseScraper.js
+++ backend/images/houses/houseScraper.js
@@ -1,33 +1,47 @@
-const { encode, decode } = require('node-base64-image');
+const Jimp = require("jimp");
+const chalk = require("chalk");
+const ora = require("ora");
+const figlet = require("figlet");
+const clear = require("clear");
 
-
+const throbber = ora({ spinner: "arc" });
 const url = (index) => `https://thisrentaldoesnotexist.com/img-new/img${index}.jpg`;
-const options = {
-  string: true,
-  headers: {
-    "User-Agent": "my-app"
-  }
-};
 
-function sleep(ms) {
+/**
+ * sets a time out for the given milliseconds
+ * @param {Integer} ms milliseconds to sleep
+ */
+function sleep(ms = 0) {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
 }
 
+/**
+ * Generates 385 images of houses from 
+ * thisrentaldoesnottexist.com
+ */
 async function generateImages() {
-  for (let index = 1; index <386 ; ) {
+  clear();
+  console.log(chalk.yellow(figlet.textSync("house-scraper"))+"\n");
+  for (let index = 1; index < 386 ; ) {
     await sleep(3000);
     [1,2,3,4].map((i) => fetchAndWriteImage(url(i), index++))
   }
+  throbber.success("Written all the house images!");
 }
 
+/**
+ * Fetches one image and writes it
+ * with the format 'house-image-####.jpg'
+ * @param {*} url 
+ * @param {*} index 
+ */
 async function fetchAndWriteImage(url, index) {
-    encode(url, options)
-      .then(image => {
-        console.log("Image ", index);
-        decode(image, { fname: `house-image-${(index+"").padStart(4, "0")}`, ext: 'jpg' });
-    });
+    const fileName = `house-image-${(index+"").padStart(4, "0")}.jpg`;
+    const image = await Jimp.read(url);
+    throbber.start("writing " + fileName);
+    await image.writeAsync(fileName);
 }
 
 generateImages();
