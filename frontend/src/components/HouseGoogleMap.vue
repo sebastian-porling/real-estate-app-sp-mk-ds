f2428e7cd23b74018ec7c5f0983c48e406116693
Sebastian Porling <sebastian.porling@gmail.com>
2020-08-30 18:48:23 +0200

    feat: map is now component

---
+++ frontend/src/components/HouseGoogleMap.vue
@@ -0,0 +1,14 @@
+<template>
+    <GmapMap :center="position" :zoom="10" class="maps z-depth-1">
+        <GmapMarker :position="position" />
+    </GmapMap>
+</template>
+
+<script>
+export default {
+    name: "HouseGoogleMap",
+    props: ["position"]
+};
+</script>
+
+<style></style>
