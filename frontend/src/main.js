2912d7c6affe0b99e8a1a8e3127a6f968c512cdd
Sebastian Porling <sebastian.porling@gmail.com>
2020-08-30 18:44:22 +0200

    feat: mv router functions, add global server var

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,40 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import VueRouter from "vue-router";
+import router from "./router";
+import * as VueGoogleMaps from "vue2-google-maps";
 
-import VueRouter from 'vue-router';
-
-import HouseForSale from "./components/HouseForSale.vue";
-import HousesForSaleList from './components/HousesForSaleList.vue';
-import AgentListing from "./components/AgentListing.vue";
-import AddHouseForAgent from "./components/AddHouseForAgent.vue";
-
-import * as VueGoogleMaps from 'vue2-google-maps';
+Vue.prototype.$server = 'http://localhost:3000';
 Vue.use(VueGoogleMaps, {
-  load: {
-    key: 'AIzaSyC4ORXbSRauP16RXMXzXc-dCysYrGW93hM'
-  }
+    load: {
+        key: "AIzaSyC4ORXbSRauP16RXMXzXc-dCysYrGW93hM"
+    }
 });
 
 Vue.use(VueRouter);
-
-Vue.config.productionTip = false
-
-
-const routes = [
-
-  {path: '/house/:id',name:'House', component: HouseForSale},
-  {path: '/', component: HousesForSaleList},
-  {path: '/agent/:id', name:'Agent', component: AgentListing},
-  {path: '/agent/:id/addHouse', name:'AddHouse', component: AddHouseForAgent}
-
-]
-
-const router = new VueRouter({
-  routes,
-  mode:"history"
-})
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: h => h(App),
-}).$mount('#app')
+    router,
+    render: h => h(App)
+}).$mount("#app");
