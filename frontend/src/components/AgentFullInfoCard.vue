4934d9a9d29caf526a9440c853131fe3d3c809b0
Sebastian Porling <sebastian.porling@gmail.com>
2020-08-30 18:49:13 +0200

    feat: agent info is now components

---
+++ frontend/src/components/AgentFullInfoCard.vue
@@ -0,0 +1,36 @@
+<template>
+    <router-link
+        v-if="agent !== undefined"
+        class="estateAgentInfo"
+        :to="{
+            name: 'Agent',
+            params: { id: agent.id }
+        }">
+        <div class="agentWrapper d-flex align-items-center">
+            <img v-bind:src="agent.profile_image" class="agent-img z-depth-1" />
+            <div class="p-4">
+                <h2>{{ agent.first_name }} {{ agent.last_name }}</h2>
+                <a :href="'mailto:' + agent.email">{{ agent.email }}</a>
+                <p>{{ agent.telephone }}</p>
+            </div>
+        </div>
+    </router-link>
+</template>
+
+<script>
+export default {
+    name: "AgentFullInfoCard",
+    props: ["agent"]
+};
+</script>
+
+<style>
+.agent-img {
+    max-height: 9rem;
+    max-width: 9rem;
+    border-radius: 50%;
+}
+.agent-img:hover {
+    opacity: 0.8;
+}
+</style>
---
+++ frontend/src/components/AgentShortInfoCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <router-link
+        v-if="agent !== undefined"
+        class="agentInfo text-center"
+        :to="{
+            name: 'Agent',
+            params: { id: agent.id }
+        }"
+    >
+        <div class="p-2">
+            <img v-bind:src="agent.profile_image" class="z-depth-1" />
+            <p class="mt-2">{{ agent.first_name }} {{ agent.last_name }}</p>
+        </div>
+    </router-link>
+</template>
+
+<script>
+export default {
+    name: "AgentInfoCard",
+    props: ["agent"]
+};
+</script>
+
+<style scoped>
+img {
+    margin: 10 !important;
+    height: 120px;
+    width: 120px;
+    border-radius: 50%;
+}
+</style>
