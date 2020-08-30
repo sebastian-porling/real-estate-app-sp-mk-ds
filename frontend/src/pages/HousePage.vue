0f7e47dc8a762ca953247b5091973b9023a98607
Sebastian Porling <sebastian.porling@gmail.com>
2020-08-30 18:45:28 +0200

    feat: components are now pages

---
+++ frontend/src/pages/HousePage.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="agentContainer p-2">
+        <back-button />
+        <div v-if="house">
+            <house-carousel :images="house.images" />
+            <div class="z-depth-1 p-2 pb-4 mt-4">
+                <div id="house-info" class="p-1 mt-2 mb-2">
+                    <h2>{{ house.address }}</h2>
+                    <div>{{ house.description }}</div>
+                </div>
+
+                <div class="d-flex justify-content-end">
+                    <agent-full-info-card :agent="house.agent" />
+                </div>
+            </div>
+            <house-google-map
+                :position="{ lat: house.latitude, lng: house.longitude }"
+            />
+        </div>
+    </div>
+</template>
+<script>
+import axios from "axios";
+
+export default {
+    name: "HouseForSale",
+    data() {
+        return {
+            house: null,
+            houseId: null
+        };
+    },
+    components: {
+        BackButton: () => import("../components/BackButton"),
+        HouseGoogleMap: () => import("../components/HouseGoogleMap"),
+        HouseCarousel: () => import("../components/HouseCarousel"),
+        AgentFullInfoCard: () => import("../components/AgentFullInfoCard")
+    },
+    /**
+     * Get the house id from the router
+     * Get the house data
+     */
+    async created() {
+        this.houseId = this.$route.params.id;
+        await this.getHouseObject();
+    },
+    methods: {
+        /**
+         * Get the house data
+         */
+        getHouseObject: async function() {
+            try {
+                let response = await axios.get(
+                    `${this.$server}/api/house/${this.houseId}`
+                );
+                this.house = response.data;
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
+};
+</script>
+
+<style scoped>
+header {
+    margin: 10px;
+}
+
+.agent {
+    margin-top: 5px;
+}
+.agentContainer {
+    max-width: 850px;
+    margin: 0 auto;
+}
+.maps {
+    height: 360px;
+    width: stretch;
+}
+</style>
---
+++ frontend/src/pages/HousesForSalePage.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="list" v-if="houseData">
+        <h1 class="display-3">Houses for sale!</h1>
+        <house-list :houses="houseData"> </house-list>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+import debounce from "lodash.debounce";
+export default {
+    data() {
+        return {
+            houseData: null,
+            page: 0
+        };
+    },
+    components: {
+        HouseList: () => import("../components/HouseList")
+    },
+    /**
+     * Get all house data
+     * Setup event listener for scrolling
+     * to fetch more houses
+     */
+    async created() {
+        this.houseData = await this.fetchHouses();
+        this.handleDebouncedScroll = debounce(this.handleScroll, 500);
+        window.addEventListener("scroll", this.handleDebouncedScroll);
+    },
+    methods: {
+        /**
+         * Fetch houses at the page and increment page
+         */
+        async fetchHouses() {
+            try {
+                const res = await axios.get(
+                    `${this.$server}/api/houses/${this.page++}`
+                );
+                return res.data;
+            } catch (e) {
+                this.page = 0;
+                return [];
+            }
+        },
+        /**
+         * When the page is 15% at the bottom
+         * fetch more houses
+         */
+        async handleScroll() {
+            if (
+                window.innerHeight + window.scrollY >=
+                document.body.offsetHeight * 0.85
+            ) {
+                const data = await this.fetchHouses();
+                this.houseData.push(...data);
+            }
+        }
+    }
+};
+</script>
+
+<style scoped>
+.list {
+    max-width: 850px;
+    padding: 20px;
+    margin: 0 auto;
+}
+
+img {
+    margin: 10 !important;
+    height: 120px;
+    width: 120px;
+    border-radius: 50%;
+}
+</style>
