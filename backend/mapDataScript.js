3fe8c9943727a2e0b6f01836a6767fa4a8edc059
Sebastian Porling <sebastian.porling@gmail.com>
2020-08-31 09:04:25 +0200

    update: new url format

--- backend/mapDataScript.js
+++ backend/mapDataScript.js
@@ -1,12 +1,12 @@
 const jf = require("jsonfile"),
       shortid = require("shortid"),
-      FILE = "./data/house_json",
+      FILE = "./data/house_data.json",
       data = require(FILE),
       FORMATING = { spaces: 4, EOL: "\r\n" },
       profileImage = (id) =>
-        `http://localhost:3000/image/profile/agent-profile-${(id + "").padStart(3, "0")}.jpg`,
+        `/image/profile/agent-profile-${(id + "").padStart(3, "0")}.jpg`,
       houseImage = (id) =>
-        `http://localhost:3000/image/houses/house-image-${(id + "").padStart(4, "0")}.jpg`;
+        `/image/houses/house-image-${(id + "").padStart(4, "0")}.jpg`;
 
 /**
  *  Script that adds ids for houses,
@@ -21,7 +21,7 @@ function mapDataWithIdAndImages() {
         for (const house of agent.listings) {
             newHouse = { id: shortid.generate(), ...house };
             for (const image in newHouse.images) {
-                    newHouse[image];
+                    newHouse.images[image] = houseImage(h);
                     h++;
             }
             newListings.push(newHouse);
